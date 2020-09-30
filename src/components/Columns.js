2be195e4f0e9a6e5bcba5bc298621fd5f3311871
Emiel de Jong <emieldejong@xs4all.nl>
2020-09-30 14:00:37 +0200

    lesson 25

---
+++ src/components/Columns.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Columns() {
+    const items = []
+    return (
+        <>
+            <td>name</td>
+            <td>Emiel</td>
+        </>
+    )
+}
+
+export default Columns

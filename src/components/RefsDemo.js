eab38594d64642da03f1aa4071e16ca4fd1cc4be
Emiel de Jong <emieldejong@xs4all.nl>
2020-10-01 08:36:16 +0200

    lesson 29

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -5,11 +5,18 @@ class RefsDemo extends Component {
     constructor(props) {
         super(props)
         this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCbRef = (element) => {
+            this.cbRef = element
+        }
     }
 
     componentDidMount() {
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        if (this.cbRef) {
+            this.cbRef.focus();
+        }
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
     }
 
     clickHandler = () => {
@@ -20,6 +27,7 @@ class RefsDemo extends Component {
         return (
             <div>
                 <input type='text' ref={this.inputRef} />
+                <input type='text' ref={this.setCbRef} />
                 <button onClick={this.clickHandler} >Click</button>
             </div>
         )
